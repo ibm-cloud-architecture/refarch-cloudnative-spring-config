56186fbfbce219330c308b48365e7eb755101dc1
edwin van de grift <edwinvandegrift@us.ibm.com>
2016-10-04 12:12:49 -0700

    Enable discovery

--- src/main/java/hello/ConfigServerApplication.java
+++ src/main/java/hello/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.ibm.microservices.wfd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
